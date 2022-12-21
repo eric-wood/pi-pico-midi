3bfc754b04a1bfd873139a49a3cbe6e70ada5172
Eric Wood <eric@ericwood.org>
2022-12-21 14:23:00 -0700

    simplify IRQ

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,73 @@
-//! # Pico Blinky Example
+//! # UART IRQ Echo Example
 //!
-//! Blinks the LED on a Pico board.
+//! This application demonstrates how to use the UART Driver to talk to a serial
+//! connection. In this example, the IRQ owns the UART and you cannot do any UART
+//! access from the main thread.
 //!
-//! This will blink an LED attached to GP25, which is the pin the Pico uses for
-//! the on-board LED.
+//! The pinouts are:
+//!
+//! * GPIO 0 - UART TX (out of the RP2040)
+//! * GPIO 1 - UART RX (in to the RP2040)
+//! * GPIO 25 - An LED we can blink (active high)
 //!
 //! See the `Cargo.toml` file for Copyright and license details.
 
 #![no_std]
 #![no_main]
 
+// These are the traits we need from Embedded HAL to treat our hardware
+// objects as generic embedded devices.
+use embedded_hal::{
+    digital::v2::OutputPin,
+    serial::{Read, Write},
+};
+
+// We also need this for the 'Delay' object to work.
+use hal::Clock;
+// use rp_pico::hal::Clock;
+
 // The macro for our start-up function
 use rp_pico::entry;
 
-// GPIO traits
-use embedded_hal::digital::v2::OutputPin;
+// Time handling traits
+use fugit::RateExtU32;
 
 // Ensure we halt the program on panic (if we don't mention this crate it won't
 // be linked)
 use panic_halt as _;
 
-// Pull in any important traits
-use rp_pico::hal::prelude::*;
+// Alias for our HAL crate
+// use rp2040_hal as hal;
+use rp_pico::hal;
 
 // A shorter alias for the Peripheral Access Crate, which provides low-level
 // register access
-use rp_pico::hal::pac;
+use hal::pac;
 
-// A shorter alias for the Hardware Abstraction Layer, which provides
-// higher-level drivers.
-use rp_pico::hal;
+// Our interrupt macro
+use hal::pac::interrupt;
+
+// Some short-cuts to useful types
+use core::cell::RefCell;
+use critical_section::Mutex;
+
+// UART related types
+use hal::uart::{DataBits, StopBits, UartConfig};
+
+/// External high-speed crystal on the Raspberry Pi Pico board is 12 MHz. Adjust
+/// if your board has a different frequency
+const XTAL_FREQ_HZ: u32 = 12_000_000u32;
+
+type InterruptFlag = Mutex<RefCell<bool>>;
+static UART_RX_INTERRUPT: InterruptFlag = Mutex::new(RefCell::new(false));
 
 /// Entry point to our bare-metal application.
 ///
 /// The `#[entry]` macro ensures the Cortex-M start-up code calls this function
 /// as soon as all global variables are initialised.
 ///
-/// The function configures the RP2040 peripherals, then blinks the LED in an
-/// infinite loop.
+/// The function configures the RP2040 peripherals, then writes to the UART in
+/// an infinite loop.
 #[entry]
 fn main() -> ! {
     // Grab our singleton objects
@@ -48,10 +78,8 @@ fn main() -> ! {
     let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
 
     // Configure the clocks
-    //
-    // The default is to generate a 125 MHz system clock
     let clocks = hal::clocks::init_clocks_and_plls(
-        rp_pico::XOSC_CRYSTAL_FREQ,
+        XTAL_FREQ_HZ,
         pac.XOSC,
         pac.CLOCKS,
         pac.PLL_SYS,
@@ -62,14 +90,13 @@ fn main() -> ! {
     .ok()
     .unwrap();
 
-    // The delay object lets us wait for specified amounts of time (in
-    // milliseconds)
+    // Lets us wait for fixed periods of time
     let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
 
     // The single-cycle I/O block controls our GPIO pins
     let sio = hal::Sio::new(pac.SIO);
 
-    // Set the pins up according to their function on this particular board
+    // Set the pins to their default state
     let pins = rp_pico::Pins::new(
         pac.IO_BANK0,
         pac.PADS_BANK0,
@@ -77,14 +104,61 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
 
-    // Set the LED to be an output
+    let uart_pins = (
+        // UART TX (characters sent from RP2040) on pin 1 (GPIO0)
+        pins.gpio0.into_mode::<hal::gpio::FunctionUart>(),
+        // UART RX (characters received by RP2040) on pin 2 (GPIO1)
+        pins.gpio1.into_mode::<hal::gpio::FunctionUart>(),
+    );
+
+    // Make a UART on the given pins
+    let mut uart = hal::uart::UartPeripheral::new(pac.UART0, uart_pins, &mut pac.RESETS)
+        .enable(
+            UartConfig::new(31250.Hz(), DataBits::Eight, None, StopBits::One),
+            clocks.peripheral_clock.freq(),
+        )
+        .unwrap();
+
+    unsafe {
+        // Enable the UART interrupt in the *Nested Vectored Interrupt
+        // Controller*, which is part of the Cortex-M0+ core.
+        pac::NVIC::unmask(hal::pac::Interrupt::UART0_IRQ);
+    }
+
+    // Tell the UART to raise its interrupt line on the NVIC when the RX FIFO
+    // has data in it.
+    uart.enable_rx_interrupt();
+
+    // But we can blink an LED.
     let mut led_pin = pins.led.into_push_pull_output();
 
-    // Blink the LED at 1 Hz
     loop {
-        led_pin.set_high().unwrap();
-        delay.delay_ms(500);
-        led_pin.set_low().unwrap();
-        delay.delay_ms(500);
+        // The normal *Wait For Interrupts* (WFI) has a race-hazard - the
+        // interrupt could occur between the CPU checking for interrupts and
+        // the CPU going to sleep. We wait for events (and interrupts), and
+        // then we set an event in every interrupt handler. This ensures we
+        // always wake up correctly.
+        cortex_m::asm::wfe();
+
+        let uart_rx = critical_section::with(|cs| UART_RX_INTERRUPT.borrow(cs).replace(false));
+
+        if uart_rx {
+            // Light the LED to indicate we saw an interrupt.
+            led_pin.set_high().unwrap();
+            while let Ok(byte) = uart.read() {
+                let _ = uart.write(byte);
+            }
+            delay.delay_ms(100);
+            led_pin.set_low().unwrap();
+        }
     }
+}
+
+#[interrupt]
+fn UART0_IRQ() {
+    critical_section::with(|cs| UART_RX_INTERRUPT.borrow(cs).replace(true));
+
+    // Set an event to ensure the main thread always wakes up, even if it's in
+    // the process of going to sleep.
+    cortex_m::asm::sev();
 }
